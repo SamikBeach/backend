d548d36464fc6ff63f34eab0b0ce4d54bc5868af
bong <jungcome7@gmail.com>
2024-12-26 11:33:09 +0900

    feat: 스웨거 모듈 셋업

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('samik beach')
+    .setDescription('samik beach API description')
+    .setVersion('2.0')
+    .addServer('/api/v2')
+    .addBasicAuth()
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
