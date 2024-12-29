00afec7be958c710eec94d3d0e37293ce7791e2d
bong <jungcome7@gmail.com>
2024-12-29 12:26:17 +0900

    feat: cors origin 설정

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { VersioningType } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: { credentials: true, origin: 'http://localhost:3000' },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('samik beach')
