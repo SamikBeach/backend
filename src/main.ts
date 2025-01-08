6e0c68ef6662e876685047ac99f3526377d0c913
bong <jungcome7@gmail.com>
2025-01-08 14:30:32 +0900

    feat: 외부 노출 필드 제한

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -47,6 +49,10 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3001);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 3001;
+  await app.listen(port);
 }
 bootstrap();
